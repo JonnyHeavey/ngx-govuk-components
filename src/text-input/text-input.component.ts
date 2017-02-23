be900af301a5eb76e365a0dff2787bb93b091d58
JonnyHeavey <jonny.heavey@surevine.com>
2017-02-23 15:52:10 +0000

    removed redundant comments from textinput class

--- src/text-input/text-input.component.ts
+++ src/text-input/text-input.component.ts
@@ -37,20 +37,15 @@ export class TextInputComponent implements ControlValueAccessor {
 
   constructor() {}
 
-  //The internal data model
   private innerValue: string = '';
 
-  //Placeholders for the callbacks which are later providesd
-  //by the Control Value Accessor
   private onTouchedCallback: () => void = noop;
   private onChangeCallback: (_: any) => void = noop;
 
-  //get accessor
   get value(): any {
       return this.innerValue;
   };
 
-  //set accessor including call the onchange callback
   set value(v: any) {
       if (v !== this.innerValue) {
           this.innerValue = v;
@@ -58,24 +53,16 @@ export class TextInputComponent implements ControlValueAccessor {
       }
   }
 
-  //Set touched on blur
-  onBlur() {
-      this.onTouchedCallback();
-  }
-
-  //From ControlValueAccessor interface
   writeValue(value: any) {
       if (value !== this.innerValue) {
           this.innerValue = value;
       }
   }
 
-  //From ControlValueAccessor interface
   registerOnChange(fn: any) {
       this.onChangeCallback = fn;
   }
 
-  //From ControlValueAccessor interface
   registerOnTouched(fn: any) {
       this.onTouchedCallback = fn;
   }
